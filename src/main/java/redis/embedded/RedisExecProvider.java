aa64395dc6abb7c9b73c43b71838f852222d5ac5
DESKTOP-7UGG3FI\bai <248457006@qq.com>
2025-01-14 22:08:40 +0800

    1. 修改版本号

--- src/main/java/redis/embedded/RedisExecProvider.java
+++ src/main/java/redis/embedded/RedisExecProvider.java
@@ -13,40 +13,46 @@ import java.io.IOException;
 import java.util.Map;
 
 public class RedisExecProvider {
-    
+
     public static final String redisVersion = "7.0.15";
     //(windows-version)https://github.com/zkteco-home/redis-windows/blob/master/redis-server.exe
     public static final String redisVersionWindows = "7.2.5";
     private final Map<OsArchitecture, String> executables = Maps.newHashMap();
-    
+
+
     private String dataPath = null;
-    
+
     private RedisExecProvider() {
         initExecutables();
     }
-    
+
     public static RedisExecProvider defaultProvider() {
         return new RedisExecProvider();
     }
-    
+
     private void initExecutables() {
         executables.put(OsArchitecture.UNIX_x86, "redis-server-" + redisVersion + "-linux-386");
         executables.put(OsArchitecture.UNIX_x86_64, "redis-server-" + redisVersion + "-linux-amd64");
         executables.put(OsArchitecture.UNIX_arm64, "redis-server-" + redisVersion + "-linux-arm64");
-        
+
         executables.put(OsArchitecture.MAC_OS_X_x86_64, "redis-server-" + redisVersion + "-darwin-amd64");
         executables.put(OsArchitecture.MAC_OS_X_arm64, "redis-server-" + redisVersion + "-darwin-arm64");
-        
+
         executables.put(OsArchitecture.WIN_x64_86, "redis-server-" + redisVersionWindows + "-windows-amd64.exe");
-        
+
     }
-    
+
     public RedisExecProvider setDataDirectory(String dataPath) {
         Preconditions.checkNotNull(dataPath);
         this.dataPath = dataPath;
         return this;
     }
-    
+
+    public String getDataPath() {
+        return dataPath;
+    }
+
+
     public RedisExecProvider override(OS os, String executable) {
         Preconditions.checkNotNull(executable);
         for (Architecture arch : Architecture.values()) {
@@ -54,28 +60,28 @@ public class RedisExecProvider {
         }
         return this;
     }
-    
+
     public RedisExecProvider override(OS os, Architecture arch, String executable) {
         Preconditions.checkNotNull(executable);
         executables.put(new OsArchitecture(os, arch), executable);
         return this;
     }
-    
+
     public File get() throws IOException {
         OsArchitecture osArch = OsArchitecture.detect();
-        
+
         if (!executables.containsKey(osArch)) {
             throw new IllegalArgumentException("No Redis executable found for " + osArch);
         }
-        
+
         String executablePath = executables.get(osArch);
         if (dataPath == null) {
             dataPath = Files.createTempDir().getAbsolutePath();
         }
         return fileExists(executablePath) ? new File(executablePath) : JarUtil.extractExecutableFromJar(dataPath, executablePath);
-        
+
     }
-    
+
     private boolean fileExists(String executablePath) {
         return new File(executablePath).exists();
     }
